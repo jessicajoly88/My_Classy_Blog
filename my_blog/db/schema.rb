769f836a08185121d9285ba7d79781e014528525
Jessica <jessicajoly88@gmail.com>
2015-10-16 10:54:01 -0700

    Unit tests for Post class pass which includes validation for presence of title and body.

--- my_blog/db/schema.rb
+++ my_blog/db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151016174108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "rating"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
