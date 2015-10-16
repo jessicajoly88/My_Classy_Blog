c1494838e039d6ef75e11d4038449e3c4d5f711e
Jessica <jessicajoly88@gmail.com>
2015-10-16 13:54:19 -0700

    Create many to many relationship between tags an posts and unit specs pass.

--- my_blog/db/schema.rb
+++ my_blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016203330) do
+ActiveRecord::Schema.define(version: 20151016204622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20151016203330) do
     t.string   "body"
     t.integer  "rating"
     t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts_tags", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
