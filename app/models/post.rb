ebf5fbebf983523de8bf10ca81d776d97a2d0041
Jessica <jessicajoly88@gmail.com>
2015-10-23 16:52:23 -0700

    Update README file.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
   has_many :comments 
-  
   has_and_belongs_to_many :tags
-
-  
   
   validates :title, :presence => true
   validates :body, :presence => true
