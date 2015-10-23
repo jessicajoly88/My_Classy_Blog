bc1b8e583c19dd671e56b714becc656c040768f5
Jessica <jessicajoly88@gmail.com>
2015-10-23 13:24:03 -0700

    Add functionality to edit a comment on post page and integration spec passes.

--- spec/features/comment_pages_spec.rb
+++ spec/features/comment_pages_spec.rb
@@ -19,3 +19,17 @@ describe 'the add a comment process' do
      expect(page).to have_content 'errors'
    end
 end
+
+describe  "the edit a comment process" do
+	it "edits a comment" do
+	  post = Post.create(:title=> "This Sucks", :body=> "Hot and Steamy")
+      visit post_path(post)
+      click_on 'Leave a Comment'
+      fill_in 'Content', :with => "this is the bes thing ever ver ever"
+  	  click_on 'Post Comment'
+  	  click_on 'Edit Comment'
+  	  fill_in 'Content', :with => "this is the best thing ever"
+  	  click_on 'Post Comment'
+  	  expect(page).to have_content 'best'
+	end
+end
