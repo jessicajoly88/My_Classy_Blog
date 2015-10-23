a1d4e3ce3115c0778e8bc10caf9969a180dfec8b
Jessica <jessicajoly88@gmail.com>
2015-10-23 13:19:06 -0700

    Integration spec for error message when comment content field is left black passes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,28 @@ class CommentsController < ApplicationController
   	  end
   end 
 
+  def edit 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.find(params[:id])
+  	  if @comment.update(comment_params)
+  	  	redirect_to post_path(@comment.post)
+  	  else
+  	  	render :edit
+  	  end
+  end 
+
+  def destroy
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.find(params[:id])
+  	@comment.destroy
+  	redirect_to post_path(@post)
+  end
+
  private
 
   def comment_params
--- spec/features/comment_pages_spec.rb
+++ spec/features/comment_pages_spec.rb
@@ -9,4 +9,13 @@ describe 'the add a comment process' do
   	click_on 'Post Comment'
   	expect(page).to have_content "bes"
   end
+
+  it "gives error when content is not entered" do
+     post= Post.create(:title => "Hot and Steamy", :body => "Betrayal")
+     visit post_path(post)
+     click_on 'Leave a Comment'
+  	 fill_in 'Content', :with => " "
+  	 click_on 'Post Comment'
+     expect(page).to have_content 'errors'
+   end
 end
