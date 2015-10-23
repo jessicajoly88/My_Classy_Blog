72f67b4c35d7431e86f6207c761800430c027799
Jessica <jessicajoly88@gmail.com>
2015-10-23 13:26:25 -0700

    Add functionality to delete a comment from a post and integration spec passes.

--- spec/features/comment_pages_spec.rb
+++ spec/features/comment_pages_spec.rb
@@ -33,3 +33,15 @@ describe  "the edit a comment process" do
   	  expect(page).to have_content 'best'
 	end
 end
+
+describe "the delete a comment process" do
+	it "deletes a comment" do
+	  post = Post.create(:title=> "This Sucks", :body=> "Hot and Steamy")
+      visit post_path(post)
+      click_on 'Leave a Comment'
+      fill_in 'Content', :with => "this is the bes thing ever ver ever"
+  	  click_on 'Post Comment'
+  	  click_on 'Delete Comment'
+  	  expect(page).to_not have_content 'best'
+	end
+end
