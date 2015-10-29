597fe3dc9a20b7fda895fa0898227847382ccf3e
Jessica <jessicajoly88@gmail.com>
2015-10-29 11:04:18 -0700

    Rename user_id column in Comments table to admin_id.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   devise_for :admins
   root :to => 'posts#index'
 
   get 'static_pages/about'
 
 
-  resources :posts do 
+  resources :posts do
   	resources :tags
   	 resources :comments
   end
-  
+
     resources :tags do
   	  resources :posts
   	end
 
-    resources :users do 
-      resources :comments
-    end
 end
---
+++ db/migrate/20151029180042_rename_column_comments.rb
@@ -0,0 +1,5 @@
+class RenameColumnComments < ActiveRecord::Migration
+  def change
+    rename_column :comments, :user_id, :admin_id
+  end
+end
